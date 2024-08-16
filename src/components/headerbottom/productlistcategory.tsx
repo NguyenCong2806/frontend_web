44d1326081a7957c7a40f171a268014d96b70081
vancong <108638136+NguyenCong2806@users.noreply.github.com>
2024-08-17 06:28:16 +0700

    upcode menu

--- src/components/headerbottom/productlistcategory.tsx
+++ src/components/headerbottom/productlistcategory.tsx
@@ -3,7 +3,7 @@ const CategoryListCol = () => {
   return (
     <div className="py-2 px-3">
       <div>
-        <ul className="flex flex-row  justify-start items-center gap-10">
+        <ul className="flex flex-row flex-wrap  justify-start items-center gap-6">
           <li className="text-xs inline-block">
             <a href="#">Laptop Acer</a>
           </li>
@@ -13,7 +13,12 @@ const CategoryListCol = () => {
           <li className="text-xs inline-block">
             <a href="#">Laptop Acer</a>
           </li>
-         
+          <li className="text-xs inline-block">
+            <a href="#">Laptop Acer</a>
+          </li>
+          <li className="text-xs inline-block">
+            <a href="#">Laptop Acer</a>
+          </li>
         </ul>
       </div>
     </div>
--- src/screens/header/header.tsx
+++ src/screens/header/header.tsx
@@ -1,6 +1,8 @@
 import HeaderBottom from "../../components/headerbottom/headerbottom";
 import Onlinebuybox from "../../components/headercomponents/onlinebuybox";
 import LogoComponets from "../../components/logo/logocomponentrs";
+import HeadBodyMobile from "../../components/mobile/headbody";
+import MenuMobile from "../../components/mobile/menumobile";
 import SearchComponets from "../../components/search/searchcomponents";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -15,11 +17,13 @@ const HeaderView = () => {
             <Onlinebuybox />
           </div>
         </div>
+        <HeadBodyMobile/>
       </div>
       <div className=" bg-red-500">
         <div className="container mx-auto px-54 hidden lg:block">
           <HeaderBottom />
         </div>
+        <MenuMobile/>
       </div>
     </div>
   );
