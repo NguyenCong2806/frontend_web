a9fbe8d1569417c05e94d8b3ddd9dbdee5a53406
vancong <108638136+NguyenCong2806@users.noreply.github.com>
2024-08-18 06:13:40 +0700

    upcode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import HeaderView from "./screens/header/header";
+import SliderView from "./screens/Slider/slider";
 import Topheaderbar from "./screens/topheaderbar/top_header_bar";
 
 function App() {
   return (
     <div>
       <Topheaderbar />
-      <HeaderView/>
+      <HeaderView />
+      <SliderView />
     </div>
   );
 }
---
+++ src/screens/Slider/slider.tsx
@@ -0,0 +1,16 @@
+import SimpleSlider from "../../components/SimpleSlider/SimpleSlider";
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const SliderView = () => {
+  return (
+    <div>
+      <div className="bg-white">
+        <div className="w-full">
+          <SimpleSlider />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SliderView;
