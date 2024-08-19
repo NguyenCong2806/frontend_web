b10f8ca26a27823b747208bc4fc535147aa57866
vancong <108638136+NguyenCong2806@users.noreply.github.com>
2024-08-19 21:55:46 +0700

    upcode

--- src/components/sectionpromotion/productpromotion.tsx
+++ src/components/sectionpromotion/productpromotion.tsx
@@ -1,22 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const ProductPromotion = () => {
   return (
-    <div className="inline-block bg-slate-200 rounded-sm p-3">
-      <div>
-        <a>
-          <img src="https://maytinh3.maugiaodien.com/wp-content/uploads/2020/08/32133_tuf_gaming_h3_red_04-168x168.png"></img>
+    <div className="inline-block m-1 bg-white rounded-md p-3 mt-12 md:mt-24 relative">
+      <div className="text-center">
+        <a href="#">
+          <img className="w-56" src="https://maytinh3.maugiaodien.com/wp-content/uploads/2020/08/32133_tuf_gaming_h3_red_04-168x168.png"></img>
         </a>
       </div>
-      <div>
-        <p>
+      <div className="my-6">
+        <p className=" text-lg">
           <a href="#">Tai nghe Asus TUF H3 Red</a>
         </p>
       </div>
       <div>
-        <span>
-          <span>1.200.000 đ</span>
-          <span>900.000 đ</span>
+        <span className="font-bold space-x-2  md:space-x-4">
+          <span className="text-sm text-slate-700 line-through">1.200.000 đ</span>
+          <span className="text-slate-900">900.000 đ</span>
         </span>
+      </div>
+      <div className="absolute w-14 h-14 rounded-full bg-red-700 top-2 right-2">
+          <p className="ml-2 my-4 text-white font-bold">-18%</p>
       </div>
     </div>
   );
