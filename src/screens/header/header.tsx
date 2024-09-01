4eceaa6dac8fcc843ab769e0b1b327a39683181b
vancong <108638136+NguyenCong2806@users.noreply.github.com>
2024-09-01 08:09:41 +0700

    upcode

--- src/screens/header/header.tsx
+++ src/screens/header/header.tsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import HeaderBottom from "../../components/headerbottom/headerbottom";
 import Onlinebuybox from "../../components/headercomponents/onlinebuybox";
 import LogoComponets from "../../components/logo/logocomponentrs";
 import HeadBodyMobile from "../../components/mobile/headbody";
 import MenuMobile from "../../components/mobile/menumobile";
 import SearchComponets from "../../components/search/searchcomponents";
-
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const HeaderView = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll',handleScroll)
+  })
   return (
     <div>
       <div className="bg-white py-2">
@@ -17,13 +29,13 @@ const HeaderView = () => {
             <Onlinebuybox />
           </div>
         </div>
-        <HeadBodyMobile/>
+        <HeadBodyMobile />
       </div>
-      <div className=" bg-red-500">
+      <div className={"bg-red-500 " + (scrolled ? 'sticky-header':"")}>
         <div className="container mx-auto px-54 hidden lg:block">
           <HeaderBottom />
         </div>
-        <MenuMobile/>
+        <MenuMobile />
       </div>
     </div>
   );
